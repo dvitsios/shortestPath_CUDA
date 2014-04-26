f82da7ba263db3af1c47d2b5f40d61ec1401b6c3
Dimitrios Vitsios <dvitsios@users.noreply.github.com>
2014-04-26 17:45:12 +0100

    Update shortestPath_Cuda.cu

--- shortestPath_Cuda.cu
+++ shortestPath_Cuda.cu
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2009 by Vitsios Dimitrios
+ *  Copyright (C) 2009 by Vitsios Dimitrios, Thomaidis Panagiotis
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
